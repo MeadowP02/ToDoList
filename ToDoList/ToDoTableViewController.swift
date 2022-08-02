1865c853a1777aaa72ea99f3a5fe4e9ecd0a524f
Meadow <meadow.perez02@gmail.com>
2022-08-02 13:27:09 -0400

    Initial Commit

---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ToDo:TableViewController.swift
+//  ToDoList
+//
+//  Created by Meadow Perez on 8/2/22.
+//
+
+import UIKit
+
+class ToDo_TableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
+
+    // MARK: - Table view data source
+
+  
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important{
+            cell.textLabel?.text = "❗" + toDo.name
+        }
+        else{
+            cell.textLabel?.text = toDo.name
+        }
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+  /*  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }*/
+    
+
+}
---
+++ ToDoList/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  ToDoList
+//
+//  Created by Meadow Perez on 8/2/22.
+//
+
+import UIKit
+
+class ToDo{
+    var name = ""
+    var important = false
+}
